be64eceadb5c152adafd42ce34d45d95c3b52eb2
Jan Bernhard <janhenrikbern@users.noreply.github.com>
2020-10-06 19:44:48 -0400

    convert images from multiple directories using BFS

--- image_converter.py
+++ image_converter.py
@@ -10,17 +10,29 @@ a = parser.parse_args()
 def main():
     if not os.path.exists(a.output_dir):
         pwd = ""
-        for path_segment in os.path.split(a.output_dir):
+        for path_segment in a.output_dir.split('/'):
             pwd = os.path.join(pwd, path_segment)
             if not os.path.exists(pwd):
                 os.mkdir(pwd)
 
     cnt = 1
-    for f in os.listdir(a.input_dir):
-        if not (f.endswith(".jpg") or f.endswith("DS_Store")):
-            img = Image.open(os.path.join(a.input_dir, f))
-            img = img.convert('RGB')
-            img.save(os.path.join(a.output_dir, f"{cnt}.jpg"), quality=100)
-            cnt += 1
+    queue = [a.input_dir]
+    while queue:
+        pwd = queue.pop(0)
+        for f in os.listdir(pwd):
+            if f.endswith("DS_Store"): continue
+            f_path = os.path.join(pwd, f)
+            if os.path.isdir(f_path):
+                queue.append(f_path)
+            elif os.path.isfile(f_path) and not f.endswith(".jpg"):
+                img = Image.open(f_path)
+                img = img.convert('RGB')
+                w, h = img.size
+                if not (h ==256 and w ==256):
+                    ratio =  256 / min(h,w)
+                    img = img.resize((round(w*ratio)+1, round(h*ratio)+1))
+                    img = img.crop((0,0,256,256))
+                img.save(os.path.join(a.output_dir, f"{cnt}.jpg"), quality=100)
+                cnt += 1
 
 main()
